1c1167bc48a05936497420d9724af7573642346b
Kazuto Tashiro <ktashiro.for.work@gmail.com>
2023-04-25 15:04:36 +0900

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            int guessNumber = 1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                    guessNumber ++;
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                    guessNumber ++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You got the right number in {guessNumber}.");
+                }
+            }
+            Console.WriteLine("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while  (response == "yes");          
     }
 }
