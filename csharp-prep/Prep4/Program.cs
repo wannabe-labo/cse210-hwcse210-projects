69acc9ed8be355467394231402a137f50667d699
Kazuto Tashiro <ktashiro.for.work@gmail.com>
2023-04-25 16:41:31 +0900

    list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+      
+        int largestNumber = numbers[0];
+        int smallestPositiveNumber = numbers[0];
+        numbers.Sort();
+
+        foreach (int number in numbers)
+        {
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+            else if (number > 0 && number < smallestPositiveNumber)
+            {
+                smallestPositiveNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The largest is: {largestNumber}");
+
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
